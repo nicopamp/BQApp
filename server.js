fb51f4dda6137daeb56dffdd10e660e21cd858c7
nicopamp <nico@pampfamily.com>
2024-03-25 12:48:34 -0400

    Adding a rate-limiter

--- server.js
+++ server.js
@@ -4,8 +4,17 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// set up rate limiter: maximum of five requests per minute
+const RateLimit = require('express-rate-limit');
+const limiter = RateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // max 100 requests per windowMs
+});
+
 // Serve static files from the public directory
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(limiter)
 
 // Define a route to serve the index.html file
 app.get('/', (req, res) => {
